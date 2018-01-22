a3c96bd8ca0eb29ab6ff29e8c1d664efd8df6159
BastienCarbonnier <bastien.carbonnier@gmail.com>
2018-01-22 18:15:15 +0100

    rename app.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 8080, function () {
+server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url);
 });
 
