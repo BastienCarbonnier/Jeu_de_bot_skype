f5d5c12d10a4ac60a29d1571c21ae0578b283c12
BastienCarbonnier <bastien.carbonnier@gmail.com>
2018-01-25 16:04:58 +0100

    Version propre

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Setup Restify Server
-// Get environment variable
 var server_port = 8080;
 var server_ip_address = '0.0.0.0';
 
@@ -23,33 +22,5 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+    session.send("Voici le message reçu : %s", session.message.text);
 });
-/*
-// Create bot and add dialogs
-var connector = new builder.ChatConnector({
-    appId: "4b9732bc-4088-40f3-99b7-8bf99bebe22a",
-    appPassword: "hlTTFJ18;(zrpbaRJD691^}"
-});
-var bot = new builder.UniversalBot(connector);
-bot.dialog('/', function (session) {
-    session.send('Hello World');
-});
-
-// Setup Restify Server
-var server = restify.createServer();
-server.post('/api/messages', connector.listen());
-server.listen(8080, function () {
-    console.log('%s listening to %s', server.name, server.url);
-});
-*/
-/*
-var server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});*/
