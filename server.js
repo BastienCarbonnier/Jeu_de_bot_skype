5d387510f911b4f1680e49b513562b77024cbeb5
BastienCarbonnier <bastien.carbonnier@gmail.com>
2018-01-25 14:05:03 +0100

    Modification parametre

--- server.js
+++ server.js
@@ -15,8 +15,20 @@ server.listen(server_port,server_ip_address, function () {
    console.log( "Listening on " + server_ip_address + ", port " + server_port );
 });
 */
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: "4b9732bc-4088-40f3-99b7-8bf99bebe22a",
+    appPassword: "hlTTFJ18;(zrpbaRJD691^}"
+});
 
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
 
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
+/*
 // Create bot and add dialogs
 var connector = new builder.ChatConnector({
     appId: "4b9732bc-4088-40f3-99b7-8bf99bebe22a",
@@ -33,7 +45,7 @@ server.post('/api/messages', connector.listen());
 server.listen(8080, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
-
+*/
 /*
 var server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -44,20 +56,3 @@ var server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });*/
-
-
-/*
-// Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-
-// Listen for messages from users
-server.post('/api/messages', connector.listen());
-
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
-*/
