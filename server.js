d14b3f2be17dd63786dc23aa2501af2d1e97746c
BastienCarbonnier <bastien.carbonnier@gmail.com>
2018-01-25 10:53:29 +0100

    Modification parametre

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Get environment variable
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.OPENSHIFT_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_IP || '127.0.0.1';
 
 // Setup Restify Server
 var server = restify.createServer();
