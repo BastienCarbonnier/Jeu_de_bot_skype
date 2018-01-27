d60206f7352ad69e51301e5039d87bf6d87497d5
BastienCarbonnier <bastien.carbonnier@gmail.com>
2018-01-27 22:10:41 +0100

    Rajout variable d'environnement

--- server.js
+++ server.js
@@ -2,20 +2,16 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Setup Restify Server
-var server_port = 8080;
-var server_ip_address = '0.0.0.0';
-
 var server = restify.createServer();
-console.log(process.env.ESSAI);
-server.listen(process.env.port || process.env.PORT || 8080, function () {
+
+server.listen(process.env.PORT || 8080, function () {
    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: "cd7ddebd-7fb9-4be5-8bbd-96755ba02417",
-    appPassword: "qngHH20!mqnnFHTMN715_)~"
-
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 // Listen for messages from users
